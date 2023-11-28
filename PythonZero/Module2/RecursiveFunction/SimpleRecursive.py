204d359910c60304a598c76b446e75ddb4411825
Edcarlos <85942078+Levi-cell@users.noreply.github.com>
2023-11-28 15:40:25 -0300

    Atualizando arquivos de função recursiva

--- PythonZero/Module2/RecursiveFunction/SimpleRecursive.py
+++ PythonZero/Module2/RecursiveFunction/SimpleRecursive.py
@@ -1,10 +1,23 @@
 # Função recursiva
-def decrease(number):
-    print(number)
-    number -= 1
+# def decrease(number):
+#     print(number)
+#     number -= 1
+#
+#     if number >= 1:
+#         decrease(number)
+#
+# input_number = int(input("Enter a number: "))
+# decrease(input_number)
 
-    if number >= 1:
-        decrease(number)
 
-input_number = int(input("Enter a number: "))
-decrease(input_number)
+print(reversed(list(range(1,11))))
+
+num_list = [1, 2, 3, 4, 5]
+
+num_list.remove(2)
+
+y = "suff;thing;junk;"
+
+z = y.split(';')
+print(len(z))
+
